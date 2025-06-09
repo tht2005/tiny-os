85b0eb39b6e72d49e92cd855bfb84248a0ae11a1
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-06-09 12:26:24 +0700

    Move UART defines and symbols to headert

--- drivers/qemu-virt/uart.c
+++ drivers/qemu-virt/uart.c
@@ -1,18 +1,5 @@
 #include "uart.h"
 
-#define UART0_BASE 0x10000000
-
-#define REG(base, offset)           ((*((volatile unsigned char *)(base + offset))))
-#define UART0_DR                    REG(UART0_BASE, 0x00)
-#define UART0_FCR                   REG(UART0_BASE, 0x02)
-#define UART0_LSR                   REG(UART0_BASE, 0x05)
-
-#define UARTFCR_FFENA               0x01
-#define UARTLSR_THRE                0x20
-#define UARTLSR_DR                  0x01
-
-#define UART0_FF_THR_EMPTY           (UART0_LSR & UARTLSR_THRE)
-
 void uart_init ()
 {
     UART0_FCR = UARTFCR_FFENA;
--- include/uart.h
+++ include/uart.h
@@ -1,5 +1,19 @@
 #ifndef __UART_H
 #define __UART_H
+
+#define UART0_BASE 0x10000000
+
+#define REG(base, offset)           ((*((volatile unsigned char *)(base + offset))))
+#define UART0_DR                    REG(UART0_BASE, 0x00)
+#define UART0_FCR                   REG(UART0_BASE, 0x02)
+#define UART0_LSR                   REG(UART0_BASE, 0x05)
+
+#define UARTFCR_FFENA               0x01
+#define UARTLSR_THRE                0x20
+#define UARTLSR_DR                  0x01
+
+#define UART0_FF_THR_EMPTY           (UART0_LSR & UARTLSR_THRE)
+
 void uart_init ();
 void uart_putc (char c);
 void uart_puts (const char *str);
