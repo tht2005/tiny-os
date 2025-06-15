dd648a47bc9f214752fb06f6ff6881855d6c9452
Nguyen Dinh Dang Duong <dangduong31205@gmail.com>
2025-06-15 16:56:26 +0700

    Generated header for kernel configuration
    In the future (or my real project),
    a python (or ?) script will generate this,
    according to user config file.

---
+++ generated/autoconf.h
@@ -0,0 +1,10 @@
+#ifndef __AUTOCONF_H
+#define __AUTOCONF_H
+
+#define CONFIG_MAX_CPUS                         8
+
+#define CONFIG_PROCESS_START_ADDR               0x80000000
+#define CONFIG_PROCESS_STACK_ADDR               0x100000000
+#define CONFIG_PROCESS_STACK_PAGES              2
+
+#endif
